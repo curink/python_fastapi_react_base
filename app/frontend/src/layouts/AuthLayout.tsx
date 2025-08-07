e38f79bdecbbe8619ef277a51452383320be230a
curink <curink@gmail.com>
2025-08-07 15:05:39 +0700

    first commit

--- app/frontend/src/layouts/AuthLayout.tsx
+++ app/frontend/src/layouts/AuthLayout.tsx
@@ -1,11 +1,14 @@
 // /src/layouts/AuthLayout.tsx
 
+import { useState } from "react"
 import { Outlet } from "react-router-dom"
 
 export default function AuthLayout(): JSX.Element {
+  const [pageTitle, setPageTitle] = useState("")
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 px-4">
-      <Outlet />
+      <Outlet context={{ setPageTitle }} />
     </div>
   )
 }
